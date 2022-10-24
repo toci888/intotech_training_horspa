cd4e1c97b6ed60a384fb31e442ad03d5514e040e
Srebrny <Srebrny@DESKTOP-7JT22M3>
2022-10-24 10:44:55 +0200

    Calculator by Krzysiu

---
+++ Hornets/Intotech.Hornets.Cmd/Intotech.Hornets.Cmd/Kalkulator/CalculatorKrzysiu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.Hornets.Cmd.Kalkulator
+{
+    public static class CalculatorKrzysiu
+    {
+        public static void Calculate()
+        {
+            Console.WriteLine("Podaj działanie:");
+            string userInput = Console.ReadLine();
+
+            //wyciągnięcie liczb od stringa użytkownika
+            string[] numbers = userInput.Split(' ');
+            int first = int.Parse(numbers[0]);
+            int second = int.Parse(numbers[2]);
+
+            Console.WriteLine("-------");
+
+            if (numbers[1] == "+")
+            {
+                Console.WriteLine($"{first} {numbers[1]} {second} = {first + second}");
+            }
+            if (numbers[1] == "-")
+            {
+                Console.WriteLine($"{first} {numbers[1]} {second} = {first - second}");
+            }
+            if (numbers[1] == "*")
+            {
+                Console.WriteLine($"{first} {numbers[1]} {second} = {first * second}");
+            }
+            if (numbers[1] == "/")
+            {
+                Console.WriteLine($"{first} {numbers[1]} {second} = {first / second}");
+            }
+
+            Console.WriteLine("-------");
+        }
+    }
+}
