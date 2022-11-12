d9828bef0ed152619df0911be1a56a6cccd3835f
Robert Kulig <new.dev1975@gmail.com>
2022-11-12 11:49:08 +0100

    RobLogic.cs

---
+++ Hornets/Intotech.Hornets.Cmd/Intotech.Hornets.Bll.Sketches/ArraysShowdown/RobLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.Hornets.Bll.Sketches.ArraysShowdown
+{
+    public class RobLogic : SubjectLogic
+    {
+        public override void FilterEntities(int id, string title, string subject)
+        {
+            List<SubjectEntity> result = subjectEntities.Where(el => el.Id > 1 && el.Subject == "rek" ).ToList();
+        }
+    }
+}
