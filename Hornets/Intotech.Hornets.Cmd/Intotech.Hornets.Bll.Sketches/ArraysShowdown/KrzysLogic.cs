20cd6500f3b6b90da4a352f223906b3fd3c6b182
Srebrny <Srebrny@DESKTOP>
2022-11-12 11:52:03 +0100

    krzys

---
+++ Hornets/Intotech.Hornets.Cmd/Intotech.Hornets.Bll.Sketches/ArraysShowdown/KrzysLogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.Hornets.Bll.Sketches.ArraysShowdown
+{
+    public class KrzysLogic : SubjectLogic
+    {
+        public override void FilterEntities(int id, string title, string subject)
+        {
+            //??
+            // 
+            subjectEntities.Where(se => se.Title.Contains("bajki") || se.Subject.Contains("kota"));
+
+            // Krzys - titile 'bajki' lub subject 'kota'
+            // Robert - id > 1 i subject 'rek'
+        }
+    }
+}
